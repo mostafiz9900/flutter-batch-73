bb0ba34d2d52f3ddddd4db3aa82c8754f38e35cd
Md. Mostafizur Rahman <mostafiz9900@gmail.com>
2021-11-03 16:18:51 +0600

    app theme 22

---
+++ intro_app_theme-22/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(        
+        primarySwatch: Colors.blue,
+      ),
+      home:HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appbar:AppBar(title:Text('Home Page')),
+      body:Column(
+        
+      )
+    );
+  }
+}
